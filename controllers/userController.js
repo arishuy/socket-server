9ca4de44d6b9a9d29e3f08e80ff697b77a06d2ad
arishuy <huybui19898@gmail.com>
2023-01-12 12:44:08 +0700

    fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,16 @@ const AppError = require("../utils/appError");
 const Chat = require("../models/chatModel");
 const Message = require("../models/messageModel");
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  }
+  );
+  return newObj;
+}
+
+
 exports.getAllFriends = catchAsync(async (req, res) => {
   const friends = await User.findById(req.user._id)
     .populate("friends")
@@ -141,8 +151,16 @@ exports.deleteFriendRequest = catchAsync(async (req, res, next) => {
 });
 
 exports.updateUser = catchAsync(async (req, res, next) => {
+  const filteredBody = filterObj(req.body, "name", "email");
+  const user = await User.findByIdAndUpdate(req.user._id, filteredBody, {
+    new: true,
+    runValidators: true,
+    });
   res.status(200).json({
     status: "success",
+    data: {
+      user: user,
+    },
   });
 });      
 
